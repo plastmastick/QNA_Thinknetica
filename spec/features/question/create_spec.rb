fea7471f9af79127bda9e68eca444f61144f435a
plastmastick <nick.razum23@gmail.com>
2022-10-09 19:07:23 +0300

    rubocop add exclude spec/features for RSpec/Capybara/FeatureMethods

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -2,22 +2,22 @@
 
 require 'rails_helper'
 
-describe 'User can create question', "
+feature 'User can create question', "
   In order to get answer from a community
   As an authenticated user
   I'd like to be able to ask the question
 " do
-  let(:user) { create(:user) }
+  given(:user) { create(:user) }
 
   describe 'Authenticated user' do
-    before do
+    background do
       sign_in(user)
 
       visit questions_path
       click_on 'Ask question'
     end
 
-    it 'asks a question' do
+    scenario 'asks a question' do
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'text text text'
       click_on 'Ask'
@@ -27,14 +27,14 @@ describe 'User can create question', "
       expect(page).to have_content 'text text text'
     end
 
-    it 'asks a question with errors' do
+    scenario 'asks a question with errors' do
       click_on 'Ask'
 
       expect(page).to have_content "Title can't be blank"
     end
   end
 
-  it 'Unauthenticated user tries to ask a question' do
+  scenario 'Unauthenticated user tries to ask a question' do
     visit questions_path
     click_on 'Ask question'
 
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,16 +2,16 @@
 
 require 'rails_helper'
 
-describe 'User can sign in', "
+feature 'User can sign in', "
   In order to ask questions
   As an unauthenticated user
   I'd like to be able to sign in
 " do
-  let(:user) { create(:user) }
+  given(:user) { create(:user) }
 
-  before { visit new_user_session_path }
+  background { visit new_user_session_path }
 
-  it 'Registered user tries to sign in' do
+  scenario 'Registered user tries to sign in' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_on 'Log in'
@@ -19,7 +19,7 @@ describe 'User can sign in', "
     expect(page).to have_content 'Signed in successfully.'
   end
 
-  it 'Unregistered user tries to sign in' do
+  scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
     click_on 'Log in'
