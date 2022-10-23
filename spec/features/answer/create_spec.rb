4a955859297947db17e1aac9dc296100febcbd59
plastmastick <nick.razum23@gmail.com>
2022-10-23 10:44:53 +0300

    feat: user can attach files to answer when create

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -32,6 +32,15 @@ feature 'User can create answer', "
 
       expect(page).to have_content "Answer can't be blank"
     end
+
+    scenario 'give a answer with attached files' do
+      fill_in 'Your answer', with: 'textAnswer'
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Create'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'Unauthenticated user tries to give a answer' do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Question, type: :model do
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :body }
 
-  it 'have man  y attached files' do
-    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  it 'have many attached files' do
+    expect(described_class.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 end
