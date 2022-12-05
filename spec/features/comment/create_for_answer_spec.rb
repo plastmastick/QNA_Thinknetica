d6b7dae152d44b83bb9b05f0a97973f4a045ff4e
plastmastick <nick.razum23@gmail.com>
2022-12-05 08:23:38 +0300

    test: add comments for questions & answers

---
+++ spec/features/comment/create_for_answer_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+feature 'User can comment an answer', %q{
+  In order to express some thoughts
+  As an authenticated user
+  I'd like to be able to comment an answer
+} do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+
+  describe 'Authenticated user', js: true do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'comments the answer' do
+      within '.answer-new-comment' do
+        fill_in 'New comment', with: 'Comment body'
+        click_on 'Post comment'
+      end
+
+      expect(page).to have_content 'Comment body'
+    end
+
+    scenario 'comments the answer with errors' do
+      within '.answer-new-comment' do
+        click_on 'Send'
+      end
+
+      expect(page).to have_content "Body can't be blank"
+    end
+  end
+
+  describe 'multiple sessions', js: true do
+    scenario "answer's comment appears on other user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('other user') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        within '.answer-new-comment' do
+          fill_in 'New comment', with: 'Comment body'
+          click_on 'Send'
+        end
+
+        expect(page).to have_content 'Comment body'
+      end
+
+      Capybara.using_session('other user') do
+        expect(page).to have_content 'Comment body'
+      end
+    end
+  end
+
+  scenario 'Unauthenticated user tries to comment the answer' do
+    visit question_path(question)
+
+    expect(page).to_not have_css('.answer-new-comment')
+  end
+end
---
+++ spec/features/comment/create_for_question_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+feature 'User can comment a question', %q{
+  In order to express some thoughts
+  As an authenticated user
+  I'd like to be able to comment a question
+} do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  describe 'Authenticated user', js: true do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'comments the question' do
+      within '.question-new-comment' do
+        fill_in 'New comment', with: 'Comment body'
+        click_on 'Send'
+      end
+
+      expect(page).to have_content 'Comment body'
+    end
+
+    scenario 'comments the question with errors' do
+      within '.question-new-comment' do
+        click_on 'Send'
+      end
+
+      expect(page).to have_content "Body can't be blank"
+    end
+  end
+
+  describe 'multiple sessions', js: true do
+    scenario "question's comment appears on other user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('other user') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        within '.question-new-comment' do
+          fill_in 'New comment', with: 'Comment body'
+          click_on 'Send'
+        end
+
+        expect(page).to have_content 'Comment body'
+      end
+
+      Capybara.using_session('other user') do
+        expect(page).to have_content 'Comment body'
+      end
+    end
+  end
+
+  scenario 'Unauthenticated user tries to comment the question' do
+    visit question_path(question)
+
+    expect(page).to_not have_css('.question-new-comment')
+  end
+end
