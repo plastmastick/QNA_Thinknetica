2a5525a28af8c7f9bac590594803d3e770933541
plastmastick <nick.razum23@gmail.com>
2022-12-06 09:15:34 +0300

    test: refactor concern test for models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,13 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it_behaves_like "commentable"
+  it_behaves_like "votable"
+
   it { is_expected.to belong_to :author }
 
   it { is_expected.to have_one(:reward).dependent(:destroy) }
 
   it { is_expected.to have_many(:answers).dependent(:destroy) }
   it { is_expected.to have_many(:links).dependent(:destroy) }
-  it { is_expected.to have_many(:comments).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :body }
@@ -19,19 +21,5 @@ RSpec.describe Question, type: :model do
 
   it 'have many attached files' do
     expect(described_class.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
-  end
-
-  describe 'return answer rating base on votes' do
-    it 'with votes' do
-      resource = create(:question)
-      create(:vote, votable: resource)
-      create(:vote, votable: resource)
-
-      expect(resource.rating).to eq(-2)
-    end
-
-    it 'without votes' do
-      expect(described_class.new.rating).to eq 0
-    end
   end
 end
---
+++ spec/support/concerns/models/votable_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.shared_examples "votable" do
+  let(:model) { described_class }
+
+  it { is_expected.to have_many(:votes).dependent(:destroy) }
+
+  describe 'return resource rating base on votes' do
+    it 'with votes' do
+      resource = create(model.to_s.underscore.to_sym)
+      create(:vote, votable: resource)
+      create(:vote, votable: resource)
+
+      expect(resource.rating).to eq(-2)
+    end
+
+    it 'without votes' do
+      expect(model.new.rating).to eq 0
+    end
+  end
+end
