bbb3711ea8911919b6b0488cca51e3509697429e
plastmastick <nick.razum23@gmail.com>
2023-05-07 18:45:41 +0300

    config: gmail smtp for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_Thinknetica_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://45.8.251.242/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials[Rails.env.to_sym][:smtp][:username],
+    password: Rails.application.credentials[Rails.env.to_sym][:smtp][:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
